0fa669d3030c9382df8782a91349390fffcc7cbd
dnagis <vincent.achard@gmail.com>
2019-01-30 09:04:41 +0100

    nouveaux bme280 banggood, nvelle adresse 0x76

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 Mix de 
-esp32_bmp280_i2c (mon mien)
+	esp32_bmp280_i2c (mon mien)
 et de 
-esp-idf/examples/bluetooth/ble_adv
+	esp-idf/examples/bluetooth/ble_adv
 
 récupère la température d'un bmx280 connecté en i2c à l'esp32 et
 advertise le résultat pendant N secondes, puis dodo, remesure, readv...
+
+adresse (0x77 ou 0x76) et numéros de pins définis dans 
+	components/bmx280/include/bmx280.h
 
 pour l'instant compatible avec Bluez-BLE -> lescan (attention au filter duplicates)
 
--- components/bmx280/include/bmx280.h
+++ components/bmx280/include/bmx280.h
@@ -28,7 +28,8 @@
 
 #define OAP_BMX280_ENABLED 1
 #define OAP_BMX280_I2C_NUM 0
-#define OAP_BMX280_ADDRESS 0x77 //Alors là grosse erreur des gars de chez openair project ils avaient mis 76!!!
+//VVNX: l'adresse: se trouve sur le rpi avec i2cdetect 1. BMP280 (adafruit): 77, BME280 (banggood janvier 2019): 76
+#define OAP_BMX280_ADDRESS 0x76 
 #define OAP_BMX280_I2C_SDA_PIN 18 //VVNX****** 15 et 2 marche aussi, MAIS avec 15 et 2 j'ai l'impression que flash bloque quand VDD branché...
 #define OAP_BMX280_I2C_SCL_PIN 19 //VVNX****** Pas d'astuce particulière pour les numéros de pin...
 
